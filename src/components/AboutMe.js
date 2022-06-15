ebfc3c1b1f3013946691dbb5cf09792b1199b7a3
Jay Crisostomo <jaycris2000@gmail.com>
2022-06-15 02:08:40 -0500

    init commit

---
+++ src/components/AboutMe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './aboutme.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+function AboutMe () {
+  return <div className="aboutme" >AboutMe</div>
+}
+
+export default AboutMe
